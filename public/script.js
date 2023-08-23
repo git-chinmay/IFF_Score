dd825f4dbd907b287b8e27159ce11e7af3e13ba0
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-08-23 21:58:52 +0530

    draft-1 push

--- public/script.js
+++ public/script.js
@@ -8,65 +8,78 @@ const impactValueDisplay = document.getElementById("impactValue");
 const frequencyValueDisplay = document.getElementById("frequencyValue");
 const frustrationValueDisplay = document.getElementById("frustrationValue");
 const iffScoreDisplay = document.getElementById("iffScore");
-const calculatesubmit = document.getElementById('calculate')
+const calculatesubmit = document.getElementById('calculate');
+const addButton = document.createElement("button");
 
 
+
+// Varibale initialisations
+// Store ticket scores in an array
+let iffScoreArray = [];
+
+// To calculate the IFF Score
 function calculateTicketIFFScore(impactValue, frequencyValue, frustrationValue){
     const iffScore = (impactValue + frequencyValue) * frustrationValue;
+	console.log("calculateTicketIFFScore", iffScore);
     return iffScore
 }
 
-let impact = ""
-let freq = ""
-let frust = ""
 
+// To update the selected input value realtime
 function updateValues() {
     // Fetching the value from the input field
     const impactValue = parseInt(impactInput.value);
     const frequencyValue = parseInt(frequencyInput.value);
     const frustrationValue = parseInt(frustrationInput.value);
 
+	console.log("updateValues", impactValue);
     // For displaying the selected input parameter values
     impactValueDisplay.textContent = impactValue;
     frequencyValueDisplay.textContent = frequencyValue;
     frustrationValueDisplay.textContent = frustrationValue;
 
-    return impactValue, frequencyValue, frustrationValue
-
-
-    // Calculating the IFF score
-    // const iffScore = calculateTicketIFFScore(impactValue, frequencyValue, frustrationValue);
-    // iffScoreDisplay.textContent = iffScore;
-    // updateTicketIFFScore(iffScore);
 }
 
-impact, freq, frust = updateValues();
-console.log(impact, freq, frust)
 
-let iffScoreArray = [];
-
+// To update ticket IFF score
 function updateTicketIFFScore(iffScore) {
     const ticketNumber = ticketNumberInput.value;
     ticketNumberResult.textContent = ticketNumber || "N/A";
 
-    const ticket_score_pare = `${ticketNumber} : ${iffScore}`
-    iffScoreArray.push(ticket_score_pare)
-    
-
-    // Update IFF score logic for the ticket here
-    // For example: const ticketIFFScore = calculateTicketIFFScore(ticketNumber);
-    // Update the "iffScore" display for the ticket
-    // iffScoreDisplay.textContent = ticketIFFScore;
+    const ticket_score_pair = `${ticketNumber} : ${iffScore}`;
+    iffScoreArray.push(ticket_score_pair);
 }
+
+
+// Event listener for the Calculate button
+calculatesubmit.addEventListener("click", () => {
+    const impactValue = parseInt(impactInput.value);
+    const frequencyValue = parseInt(frequencyInput.value);
+    const frustrationValue = parseInt(frustrationInput.value);
+
+    const iffScore = calculateTicketIFFScore(impactValue, frequencyValue, frustrationValue);
+    iffScoreDisplay.textContent = iffScore;
+
+    updateTicketIFFScore(iffScore);
+    console.log(iffScoreArray);
+});
+
+// ADD button functionality
+addButton.textContent = "ADD";
+addButton.addEventListener("click", () => {
+    iffScoreArray.forEach(score => {
+        const scoreElement = document.createElement("p");
+        scoreElement.textContent = score;
+        document.body.appendChild(scoreElement);
+    });
+
+    // Clear the array after displaying the scores
+    // iffScoreArray = [];
+});
+document.body.appendChild(addButton);
+
 
 impactInput.addEventListener("input", updateValues);
 frequencyInput.addEventListener("input", updateValues);
 frustrationInput.addEventListener("input", updateValues);
-ticketNumberInput.addEventListener("input", updateTicketIFFScore);
-
-calculatesubmit.addEventListener("click", (impact, freq, frust)=>{
-    const iffScore = calculateTicketIFFScore(impactValue=impact, frequencyValue=freq, frustrationValue=frust);
-    iffScoreDisplay.textContent = iffScore;
-    updateTicketIFFScore(iffScore)
-    console.log(iffScoreArray)
-})
+// ticketNumberInput.addEventListener("input", updateTicketIFFScore);
